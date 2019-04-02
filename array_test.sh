d722f86da5ecb74cafec25fff391f621816f1f3a
Sebyeok <tngnu02@gmail.com>
2019-04-02 22:06:02 +0900

    assignment

---
+++ array_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -i i=0
+while [ 1 ]
+do
+	read -p "Enter a new word: " word
+	if [ ${word} = "list" ]; then
+		echo ${array[*]}
+		continue
+	elif [ ${word} = "quit" ]; then
+		exit 0
+	else
+		array=(${array[*]} ${word})
+	fi
+done
+
